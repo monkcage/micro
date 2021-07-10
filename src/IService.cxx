a9abee109d9b8b5008be5cd334c7d2a3943bbb85
monkcag3 <monkcag3@qq.com>
2021-07-10 04:55:07 -0400

    service node implement & test

--- src/IService.cxx
+++ src/IService.cxx
@@ -81,16 +81,41 @@ IService::~IService()
 
 void IService::Start()
 {
+    int rc = 0;
+    zmq_msg_t identity; zmq_msg_init_size(&identity, JCONF.service.size() + 1);
+    memcpy(zmq_msg_data(&identity), JCONF.service.c_str(), JCONF.service.size() + 1);
     for(uint32_t idx = 0; idx < JCONF.gateways.size(); ++idx) {
         detail::sock_t* sock = new detail::sock_t;
         sock->zsock = zmq_socket(ctx_, ZMQ_DEALER);
         sock->id = idx;
-        sock->status = NotConnected;
+        sock->status = detail::NotConnected;
+        int timeout = 100;
+        zmq_setsockopt(sock->zsock, ZMQ_RCVTIMEO, &timeout, sizeof(timeout));
         // 先开启监控线程,再进行连接
-        threads_.emplace_back(std::thread(std::bind(&IService::startMonitorThread, this, idx)));
+        //threads_.emplace_back(std::thread(std::bind(&IService::startMonitorThread, this, idx)));
         zmq_connect(sock->zsock, JCONF.gateways[idx].c_str());
-        proxyDealers_.emplace_back(sock);
+
+        // 发送当前服务名称并等待网关确认
+        zmq_msg_send(&identity, sock->zsock, 0);
+        zmq_msg_t message; zmq_msg_init(&message);
+        int more = 1;
+        size_t more_size = sizeof(more);
+        while(more != 0) {
+            rc = zmq_msg_recv(&message, sock->zsock, 0);
+            if(rc < 0) 
+                break;
+            zmq_getsockopt(sock->zsock, ZMQ_RCVMORE, &more, &more_size);  
+        }
+        if(rc >= 0) {
+            proxyDealers_.emplace_back(sock);
+        }else{
+            LOG(WARNING) << "Can not connect to gateway(" << JCONF.gateways[idx] << ")";
+        }
     }
+    if(proxyDealers_.size() < 1) { // 确保至少连接到一个网关,否则抛出错误
+        throw std::runtime_error("Cannot connect to any gateway");
+    }
+
     proxyJoint_ = zmq_socket(ctx_, ZMQ_DEALER);
     proxyRouter_ = zmq_socket(ctx_, ZMQ_ROUTER);
     proxyDealer_ = zmq_socket(ctx_, ZMQ_DEALER);
@@ -111,15 +136,6 @@ void IService::startBackendThread()
 
 void IService::startFrontendThread()
 {
-    // first: register to gateway
-    // send service name to gateway
-    for(auto&& item: proxyDealers_) {
-        zmq_msg_t identity;  zmq_msg_init_size(&identity, JCONF.service.size() + 1);
-        memcpy(zmq_msg_data(&identity), JCONF.service.c_str(), JCONF.service.size() + 1);
-        int rc = zmq_msg_send(&identity, ((detail::sock_t*)item)->zsock, 0);
-        LOG(DEBUG) << "Send service name to Gateway ret: " << rc;
-    }
-
     // second: poll in/out
     zmq_pollitem_t* items = new zmq_pollitem_t[proxyDealers_.size() + 1];
     uint32_t i;
